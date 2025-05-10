c3636947ca380d3c51a06f08d8b47eaa3e4562e5
Steazox <steazox.pro@gmail.com>
2025-05-10 15:09:32 +0200

    analytics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Nav from './components/Nav.vue';
 import NotificationManager from './components/NotificationManager.vue';
+import { Analytics } from "@vercel/analytics/next"
 </script>
 
 <template>
